6ff42d6a2f95a21f14d6cb0b7f82b1123d9d78a8
jaxfuller <jaxful@gmail.com>
2023-10-10 15:45:02 -0600

    styled location, date, temp and weather

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
   return (
     <div className="app">
         <main>
@@ -14,6 +26,18 @@ function App() {
             className="search-bar"
             placeholder="Search for a location...">
             </input>
+          </div>
+          <div className="location-box">
+            <div className="location">Breckenridge, CO</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              50*f
+            </div>
+            <div className="weather">
+              cloudy
+            </div>
           </div>
         </main>
     </div>
