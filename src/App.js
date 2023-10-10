a5292f4d6afc1e07a9c9ddfcdbbcfae7b82121a6
jaxfuller <jaxful@gmail.com>
2023-10-10 15:23:52 -0600

    add background, search bar and search bar css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+const api = {
+  key: "6b58cd06a3a4bf2869202bf00fb23e60",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <main>
+          <div className= "search-box">
+            <input 
+            type= "text"
+            className="search-bar"
+            placeholder="Search for a location...">
+            </input>
+          </div>
+        </main>
     </div>
   );
 }
